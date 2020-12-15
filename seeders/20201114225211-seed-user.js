27cf233aeb04a9c7e61be21703ca5348fc970b82
arendonb <arendonb@gmail.com>
2020-12-15 17:29:26 -0500

    First commit

---
+++ seeders/20201114225211-seed-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('users', [{
+            name: 'carlos',
+            email: 'ejemplo@gmail.com',
+            password: '$2y$08$Z4gIwOxwetV9aomHBDp1T.mNUCTy1q5agr5K5XvnRQy5ywlqDdvNi',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]);
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('users', null, {});
+
+    }
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const apiRouter = require('./routes/index');
+const bodyPArser = require('body-parser');
+const cors = require('cors');
+
+//instancia de express en mi app
+const app = express();
+app.use(cors());
+
+app.use((req,res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    next();
+});
+
+//middleware morgan para detectar peticiones
+app.use(morgan('dev'));
+app.use(bodyPArser.json());
+app.use(bodyPArser.urlencoded({ extended: true}));
+
+//primera ruta
+app.use('/api', apiRouter);
+
+//
+//
+//
+//
+
+
+app.set('PORT', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+        console.log("Estructura base del proyecto backend");
+        res.send("Estructura base del proyecto backend");
+});
+
+app.listen(app.get('PORT'), () => {
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
+})
+
+module.exports = app;
